ad156706a772177bc0a7ba42270f8c4d9d90996f
Danny Wahl <danny@iywahl.com>
2011-12-28 10:59:08 +0800

    set ALPHA

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'theme_melbourne';
-$plugin->maturity  = MATURITY_STABLE;
+$plugin->maturity  = MATURITY_ALPHA;
 $plugin->version   = 2011122800;
 $plugin->release = '2.0 (Build: 2011122800)';
 $plugin->requires  = 2011120100;
