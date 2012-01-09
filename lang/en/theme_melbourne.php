0df766009f24be74a9498a61c08c7f92a61e5332
Danny Wahl <danny@iywahl.com>
2012-01-09 21:26:13 +0800

    spruce up readme

--- lang/en/theme_melbourne.php
+++ lang/en/theme_melbourne.php
@@ -21,6 +21,26 @@
  */
 
 $string['pluginname'] = 'Melbourne';
-$string['choosereadme'] = 'Melbourne!';
+$string['choosereadme'] = '<div class="clearfix">
+	<h2>Melbourne</h2>
+        <hr>
+	<div style="float:right;margin-left:5px;">
+		<img src="melbourne/pix/screenshot.jpg" />
+	</div>
+	<div>
+		<h3>About</h3>
+		<p>Melbourne 2.2+ is a port of the Melbourne theme for Moodle 1.9.</p>
+        <p>The original Melbourne was created during a jet-lag induced all-night code sprint during the Moodle Moot 2010 in Melbourne, Australia.</p>
+		<h3>Parents</h3>
+		<p>This theme is built on Base.</p>
+		<h3>Credits</h3>
+		<p>Melbourne 2.2+ is based off of the original Melbourne 1.0 (Moodle 1.9) by me (Danny Wahl) @ <a href="http://iyware.com" target="_blank" >iyWare</a>.</p>
+		<h3>License</h3>
+		<p>This, and all other themes included in the Moodle core, is licensed under the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.</p>
+		<h3>Theme Discussion Forum:</h3>
+		<p><a href="http://moodle.org/mod/forum/discuss.php?d=192987">http://moodle.org/mod/forum/discuss.php?d=192987</a></p>
+	</div>
+</div>
+';
 $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
